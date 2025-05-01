84b4973f711bcf9b3f91d90f4c0604274a07b4f8
Lukas Beyer <lukas.beyer@juicymo.cz>
2025-05-01 19:52:00 +0200

    Enable logging

--- src/database/index.ts
+++ src/database/index.ts
@@ -14,5 +14,6 @@ export const AppDataSource = new DataSource({
   username: process.env.APP_DB_USERNAME,
   password: process.env.APP_DB_PASSWORD,
   entities: [AppUser, Post],
-  migrations
+  migrations,
+  logging: true
 });
