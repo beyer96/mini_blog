412f9fcc10b6980fc4a8826347f27056dced3b08
Lukas Beyer <lukas.beyer@juicymo.cz>
2025-05-02 13:12:47 +0200

    Prepare simple POST /posts endpoint to enable creating new posts

--- src/routes/posts.ts
+++ src/routes/posts.ts
@@ -1,5 +1,6 @@
 import { Router } from "express";
 import Post from "../database/entities/Post";
+import authenticate from "../middlewares/authenticate";
 
 const postsRouter = Router();
 
@@ -9,5 +10,14 @@ postsRouter.route("/posts")
 
     res.status(200).send({ posts });
   })
+  .post(authenticate, async (req, res) => {
+    const { title, content, published_at } = req.body;
+
+    const newPost = Post.create({ title, content, published_at, author: req.user  });
+
+    await newPost.save();
+
+    res.status(200).send({ newPost });
+  });
 
 export default postsRouter;
