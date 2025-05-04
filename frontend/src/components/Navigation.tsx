d24c05c395865129c9ace10401f38df82e55a162
Lukas Beyer <lukas.beyer@juicymo.cz>
2025-05-04 15:02:38 +0200

    Slightly improve navigation

--- frontend/src/components/Navigation.tsx
+++ frontend/src/components/Navigation.tsx
@@ -2,10 +2,25 @@ import { NavLink } from "react-router"
 
 export default function Navigation() {
   return (
-    <nav>
-      <NavLink to="/">Home</NavLink>
-      <NavLink to="/login">Login</NavLink>
-      <NavLink to="/register">Register</NavLink>
+    <nav className="flex justify-center gap-6 mt-3 text-xl">
+      <NavLink
+        className={({ isActive }) => isActive ? "font-bold text-black" : "text-gray-600 hover:text-gray-800"}
+        to="/"
+      >
+        Home
+      </NavLink>
+      <NavLink
+        className={({ isActive }) => isActive ? "font-bold text-black" : "text-gray-600 hover:text-gray-800"}
+        to="/login"
+      >
+        Login
+      </NavLink>
+      <NavLink
+        className={({ isActive }) => isActive ? "font-bold text-black" : "text-gray-600 hover:text-gray-800"}
+        to="/register"
+      >
+        Register
+      </NavLink>
     </nav>
   )
 }
