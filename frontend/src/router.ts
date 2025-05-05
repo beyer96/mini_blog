5c68555b9f3330ed741f927d50fb5b5cc0da286d
Lukas Beyer <lukas.beyer@juicymo.cz>
2025-05-05 14:27:59 +0200

    Remove fetching user from router default layouts loader

--- frontend/src/router.ts
+++ frontend/src/router.ts
@@ -1,16 +1,10 @@
 import { createBrowserRouter } from "react-router";
 import Home from "./pages/Home";
 import DefaultLayout from "./layouts/DefaultLayout";
-import AuthService from "./services/authService";
 
 const router = createBrowserRouter([
   {
     Component: DefaultLayout,
-    loader: async () => {
-      return {
-        user: await AuthService.getUser()
-      }
-    },
     children: [
       {
         path: "/",
