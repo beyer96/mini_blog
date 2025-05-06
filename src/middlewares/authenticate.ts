84af05a0ef7fa02c87527260d5059640647db92a
Lukas Beyer <lukas.beyer@juicymo.cz>
2025-05-06 14:24:45 +0200

    Change error responses inside authenticate middleware

--- src/middlewares/authenticate.ts
+++ src/middlewares/authenticate.ts
@@ -3,17 +3,18 @@ import jwt, { JwtPayload } from "jsonwebtoken";
 import AppUser from "../database/entities/AppUser";
 import { isRevoked } from "../routes/auth";
 import AuthenticationError from "../errors/AuthenticationError";
+import AuthorizationError from "../errors/AuthorizationError";
 
 const authenticate = async (req: Request, res: Response, next: NextFunction) => {
     const accessToken = req.headers.authorization?.split(" ")[1];
-    if (!accessToken || await isRevoked(accessToken)) throw new AuthenticationError({ message: "Access denied: No token provided." });
+    if (!accessToken || await isRevoked(accessToken)) throw new AuthorizationError({ message: "Unauthorized" });
 
     jwt.verify(accessToken, process.env.ACCESS_TOKEN_SECRET!, (error, _) => {
-      if (error) throw new AuthenticationError({ message: "Access denied: Invalid token." });
+      if (error) throw new AuthorizationError({ message: "Unauthorized" });
     });
 
     const user = await AppUser.findOneBy({ username: (jwt.decode(accessToken) as JwtPayload).username });
-    if (!user) throw new AuthenticationError({ message: "Access denied: Unable to authenticate." });
+    if (!user) throw new AuthorizationError({ message: "Unauthorized" });
 
     const { password_hash, ...userInfo } = user;
     req.user = userInfo;
