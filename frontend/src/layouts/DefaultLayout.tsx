5636a6633a63b1037600c3dbeadcf076cf9447a0
Lukas Beyer <lukas.beyer@juicymo.cz>
2025-05-06 15:48:29 +0200

    Get user session on initial app load

--- frontend/src/layouts/DefaultLayout.tsx
+++ frontend/src/layouts/DefaultLayout.tsx
@@ -1,11 +1,21 @@
-import { Outlet } from "react-router";
+import { Outlet, useLoaderData } from "react-router";
 import Navigation from "../components/Navigation";
+import { useEffect } from "react";
+import { useAppDispatch } from "../store";
+import { login } from "../store/userSlice";
 
 export default function DefaultLayout() {
+  const dispatch = useAppDispatch();
+  const { user } = useLoaderData();
+
+  useEffect(() => {
+    if (user) dispatch(login(user));
+  }, [dispatch, user]);
+
   return (
     <>
       <Navigation />
       <Outlet />
     </>
-  )
+  );
 }
--- frontend/src/router.ts
+++ frontend/src/router.ts
@@ -1,10 +1,16 @@
 import { createBrowserRouter } from "react-router";
 import Home from "./pages/Home";
 import DefaultLayout from "./layouts/DefaultLayout";
+import AuthService from "./services/authService";
 
 const router = createBrowserRouter([
   {
     Component: DefaultLayout,
+    loader: async () => {
+      const { user } = await AuthService.getUserSession();
+
+      return { user };
+    },
     children: [
       {
         path: "/",
