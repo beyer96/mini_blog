a6545b53cada59f5d5eaf6d55440a0bd30271654
Lukas Beyer <lukas.beyer@juicymo.cz>
2025-05-06 18:34:19 +0200

    Add return types in all methods in auth service class

--- frontend/src/services/authService.ts
+++ frontend/src/services/authService.ts
@@ -1,26 +1,33 @@
 import axiosInstance from "./axiosInstance";
 import type { User } from "../types";
 
+type loginResponse = { user: User, accessToken: string };
+type refreshResponse = { accessToken: string };
+type logoutResponse = void;
+type getUserSessionResponse = { user: User };
+
 export default class AuthService {
-  static login = async (data: { username: User["username"], password: string }) => {
+  static login = async (
+    data: { username: User["username"], password: string }
+  ): Promise<loginResponse> => {
     const response = await axiosInstance.post("/auth/login", data);
 
     return response.data;
   };
 
-  static refresh = async () => {
+  static refresh = async (): Promise<refreshResponse> => {
     const response = await axiosInstance.post("/auth/refresh");
 
     return response.data;
   }
 
-  static logout = async () => {
+  static logout = async (): Promise<logoutResponse> => {
     return await axiosInstance.post("/auth/logout");
   }
 
-  static getUser = async (): Promise<User> => {
+  static getUserSession = async (): Promise<getUserSessionResponse> => {
     const response = await axiosInstance.get("/auth/session");
 
-    return response.data as User;
+    return response.data;
   }
 }
